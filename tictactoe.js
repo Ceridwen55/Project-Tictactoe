f826a661dc306bcfd52b4c7bf890c0bfebfb5b86
Zaqzulkan <zulkanzaqzuq@gmail.com>
2024-09-09 18:54:48 +0700

    Templating

---
+++ tictactoe.js
@@ -0,0 +1,19 @@
+// CREATING TIC-TAC-TOE GAME
+
+//Specifications :
+// a. There is a board consisted of 9 small square-space inside it
+// b. There is 2 variable of X and O to use for each players
+// c. Player move after each other
+// d. If there are 3 consecutive shapes diagonally, vertically, horizontally on the board, the player who has the shape win the game
+
+// How to work this thing ( mainly using object, function factories, IIFE)
+
+// 1. Create the asset related to the game ( board, players ) with using HTML and connect the brain of it using JS
+// A. Creating a board function
+    // a. Make a main function of board using object (consisted of 9 square)
+
+// 2. Create the game's logic
+
+// 3. Create the game's rules
+
+// 4. Design the game
